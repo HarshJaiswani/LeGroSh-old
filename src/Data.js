c6c032b0d0c77c88c336455e7513e10fecbdd1b4
HarshJaiswani <harshjaiswani2003@gmail.com>
2021-12-16 09:29:49 +0530

    readme updated

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
 let data = {
   article1: {
     heading: "A New Beginning",
-    date: "15th September 2021",
+    date: "1st September 2021",
     post: {
       part1:
         "Hey, In this huge bundles of blog already having a separate fan base who am I ?",
@@ -24,27 +24,26 @@ let data = {
     },
   },
   article2: {
-    heading: "A New Beginning",
+    heading: "Sharing the Learning",
     date: "15th September 2021",
     post: {
       part1:
-        "Hey, In this huge bundles of blog already having a separate fan base who am I ?",
+        "Hey learners, This is harsh jaiswani welcome or welcome back to the place. Today is the first episode of the sharing_the_learning. This is going to be an introductory blog.",
       part2:
-        "(That no one knows we are given birth to figure it out!) Okay keeping philosophy aside ",
+        "Who am I ?",
       part3:
-        "I am one of those who are struggling to find their dreams and make them true ,",
+        "I am a 12th pass out student who is ready to explore the new world called college. I am from science background and going to persue btech in c.s as my undergraduate degree besides my educational qualification I am one of those hustlers trying to figure out what i am supposed to do with my life as being a part of this digital generation we all had vast exposure about opportunities , fields , working areas and especially the social media all thanks to the worst and best fellow our internet",
       part4:
-        "yes talking about us all we all want that. Currently I am exploring some skills which includes web development , graphic designing and pursuing Btech in computer science ",
+        "What is sharing the learning ?",
       part5:
-        "frankly speaking to all the same question struck in mind that why will people be reading my articles in the vast ocean where there are millions already but , ",
+        "Sharing the learning is a series of episodes in which I will be sharing my day to day learning being blessed as a good student in studies later on I realized that there are same tips and tricks followed to be a good in studies with the focused study as well and as we watch youtube videos about there I realized some how unknowingly I followed them and this was because of learning from the situation to life is a great teacher we all have read these quotes so life teaches us so many things in our day to day life which if followed even the 10% we can live our dreams life so this series is going to be an attempt to make myself realize as well as share my experience although I haven’t done anything big yet but just to share those small learning",
       part6:
-        "I decided to do it not to have a large crowd in my articles I wish that happens but the primary goal is to reach out to those who are sharing same page with me. ",
-      part7:
-        "I may have a bit of knowledge to make their journey a little smoother and also because I wanted to brush up my writing skills.",
+        "we are moving forward in life we experience explore things and get a learning I am going to share so as to remind myself to take lesson and to help everyone learn from my mistakes or achievements “to human is to err” don’t be afraid to commit mistakes but be terrified of not learning from them and repeating them if you are fond of reading you can have link of documented form of this episode below in the description. I will see you in the next one till then keep learning keep ssharing.",
+      part7: "",
       part8:
-        "I hope I bought about a little positive change in your life if you are reading this uptill here.",
+        "",
       part9:
-        "It’s a humble request write a comment down about the experience of reading me for the first time weather it be a criticism or applause both will be appreciated have a great time ahead!",
+        "",
     },
   },
   article3: {
